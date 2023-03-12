7d7c81df52d1b9136aacbf8a802e750140bf5a73
litzycabrera <litzy.prinss1@gmail.com>
2023-03-12 17:09:12 +0100

    Check

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 
-function App() {
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+import React from "react";
+
+function Logo() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <header>
+      <h1>Welcome To Pokedex of Litzy</h1>
+      <img
+        src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png"
+        alt="Pokemon Pidgeot"
+      />
+    </header>
   );
 }
+
+function BestPokemon() {
+  return <p>Mi Pokemon favorito eres tú.</p>;
+}
+
+const App =()=>{
+  return (
+    <div>
+      <Logo/>
+      <BestPokemon/>
+    </div>
+  );
+};
 
 export default App;
